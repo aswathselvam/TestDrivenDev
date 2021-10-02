d3d1417f88c4e33b55cab6a4e9e7b314f24fd0d2
prat33k-dev <bhujbalpratik97@gmail.com>
2021-10-02 19:30:41 -0400

    completed the implementation and fixed the unit test

--- test/test.cpp
+++ test/test.cpp
@@ -7,7 +7,7 @@
 
 PidController pidController;
 
-// New object is initialized with custom constructor 
+// New object is initialized with custom constructor
 // to be included for code coverage.
 PidController pidController1(1, 2, 3, 1);
 
@@ -15,22 +15,19 @@ PidController pidController1(1, 2, 3, 1);
  * @brief Test for bounded output, given a large input
  */
 TEST(pidControllerTest, boundedOutput) {
-  double max = std::numeric_limits<double>::infinity();
-  ASSERT_LT(pidController.Compute(max), 1000);
+    ASSERT_LT(pidController.Compute(500), 1000);
 }
-
 /**
  * @brief Test for checking if the formula 
  * is implemented correctly
  */
 TEST(pidControllerTest, expectedValue) {
-  ASSERT_EQ(pidController.Compute(10), 10.0);
+    ASSERT_EQ(pidController.Compute(10), 10.0);
 }
-
 /**
  * @brief Test for checking if dt is initialized with 
  * non-zero and positive value
  */
 TEST(pidControllerTest, testdt) {
-  EXPECT_GT(pidController.getdt(), 0);
+    EXPECT_GT(pidController.getdt(), 0);
 }
