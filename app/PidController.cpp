687dab31d3c5a21a74f99e45278a9a5b78934149
aswathselvam <m.aswath08@gmail.com>
2021-10-01 23:10:15 -0700

    Add PID class source and header files
    PID class(PidController) .cpp and .hpp files are added

---
+++ app/PidController.cpp
@@ -0,0 +1,58 @@
+// Copyright 2021 Aswath Muthuselvam
+/**
+ *
+ * @file    PidController.cpp
+ * @author  Aswath Muthuselvam
+ * @author  Part 1 :Aswath Muthuselvam (Driver) , Prannoy Namala (Navigator)
+ * @date    10/1/2021
+ * @version 1.0
+ *
+ * @brief   PidController class implementation.
+ *
+ * @section DESCRIPTION
+ *
+ * This cpp file defines the class and methods for a PID Controller.
+ */
+
+#include <iostream>
+#include "PidController.hpp"
+
+/**
+ * @brief Constructs the PidController class object with default values
+ */
+PidController::PidController() {
+}
+
+/**
+ * @brief Constructs the PidController object with user defined values
+ */
+PidController::PidController(double Kp, double Ki, double Kd, double dt)
+    : kp_(Kp),
+      ki_(Ki),
+      kd_(Kd),
+      dt_(dt) {
+  std::cout << "User defined PID constants are " << "Kp:" << kp_ << " Ki:"
+            << ki_ << " Kd:" << kd_ << " dt:" << dt_ << std::endl;
+}
+
+/**
+ * @brief Computes the state based on target and actual state of the system
+ */
+double PidController::Compute(double targetState) {
+  // Enter your code here
+
+  return 0;
+}
+
+/**
+ * @brief This method returns the sampling time
+ */
+double PidController::getdt() {
+  return dt_;
+}
+
+/**
+ * @brief Destructor for PidController class object
+ */
+PidController::~PidController() {
+}
---
+++ include/PidController.hpp
@@ -0,0 +1,65 @@
+// Copyright 2021 Aswath Muthuselvam
+/**
+ *
+ * @file    PidController.hpp
+ * @author  Aswath Muthuselvam
+ * @author  Part 1 :Aswath Muthuselvam (Driver) , Prannoy Namala (Navigator)
+ * @date    10/1/2021
+ * @version 1.0
+ *
+ * @brief   PidController class.
+ *
+ * @section DESCRIPTION
+ *
+ * This hpp file defines the class and methods for a PID Controller.
+ */
+
+#ifndef INCLUDE_PIDCONTROLLER_HPP_
+#define INCLUDE_PIDCONTROLLER_HPP_
+
+#include <iostream>
+
+/**
+ * @brief      PID Controller Class, initialization of gain values
+ * and sampling time.
+ */
+class PidController {
+ private:
+  double kp_ = 1.0;
+  double kd_ = 0.0;
+  double ki_ = 0.0;
+  double dt_ = 0.1;
+  double currentState_ = 0;
+
+ public:
+  /**
+   * @brief Class constructor to initialize  with default values
+   */
+  PidController();
+
+  /**
+   * @brief Constructor to initialize  with user defined values
+   * @Param1: Kp is the value of proportional constant entered by user
+   * @Param2: Ki is the value of integral constant entered by user
+   * @Param3: Kd is the value of derivative constant entered by user
+   * @Param4: dt is the value sample time entered by user
+   *
+   */
+  PidController(double Kp, double Ki, double Kd, double dt);
+
+  /**
+   * @brief This method computes the magnitude of feedback for the system based 
+   *        on the current state of the system and the target setpoint set by the user
+   * @param1 targetState is the target state which needs to be achieved.
+   */
+  double Compute(double targetState);
+
+  /**
+   * @brief This method returns the sampling time dt
+   */
+  double getdt();
+
+  ~PidController();
+};
+
+#endif  // INCLUDE_PIDCONTROLLER_HPP_
