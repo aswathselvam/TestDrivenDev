dffab60a4147c8e82759fac7a47425ca7c4d6a54
aswathselvam <m.aswath08@gmail.com>
2021-10-04 16:11:32 -0400

    Update main.cpp
    Try removing content from main function as it is not used to test code and to increase code coverage

--- app/main.cpp
+++ app/main.cpp
@@ -14,38 +14,12 @@
  * This hpp file defines the class and methods for a PID Controller.
  */
 
-#include <iostream>
-#include <memory>
-#include "PidController.hpp"
 
 /**
  * @brief Main function
  * @return Program execution status
  */
 int main() {
-  /**
-   * Create an object for class Controller_PID and call the compute method.
-   */
-  // stores desired velocity, proportional gain
-  double targetState, kp, ki, kd, dt;
 
-  // integral gain, differential gain and time step.
-  std::cout << "Enter the desired proportional, integral,"
-            " and derivate gain and time-step: ";
-  std::cin >> kp >> ki >> kd >> dt;
-  // accepts parameter values and stores in the corresponding variables.
-  std::cout << "\n Enter the target state of the system: ";
-  std::cin >> targetState;  // accepts desired velocity.
-
-  // create object of class Controller_PID
-  std::shared_ptr<PidController> pidController;
-  pidController = std::make_shared<PidController>(kp, ki, kd, dt);
-
-  // call the compute method to get the calculated velocity.
-  double updatedState = pidController->Compute(targetState);
-
-  // Output the calculated velocity
-  std::cout << "The new state of the system is: " << updatedState << " m/s"
-            << std::endl;
   return 0;
 }
